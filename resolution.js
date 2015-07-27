20d267b863dcf05721c96bab1b9680088f03c423
ckhatri <chirag.k.khatri@gmail.com>
2015-07-27 13:31:57 -0500

    added privacy

--- resolution.js
+++ resolution.js
@@ -32,6 +32,16 @@ if (Meteor.isClient) {
 
     "click .toggle-checked": function() {
       Meteor.call('updateResolution', this._id, !this.checked);
+    },
+
+    "click .toggle-private": function() {
+      Meteor.call('setPrivate', this._id, !this.private);
+    }
+  });
+
+  Template.resolution.helpers({
+    isOwner : function() {
+      return this.owner === Meteor.userId();
     }
   });
 
@@ -48,7 +58,12 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish("resolutions", function() {
-    return Resolutions.find();
+    return Resolutions.find({
+      $or: [
+        {private: {$ne: true}}, 
+        {owner: this.userId}
+      ]
+    });
   });
 }
 
@@ -56,14 +71,32 @@ Meteor.methods({
   addResolution : function(title) {
     Resolutions.insert({
       title: title,
-      createdAt: new Date()
+      createdAt: new Date(),
+      owner: Meteor.userId()
     });
   },
 
   updateResolution : function(id, checked) {
+    var res = Resolutions.findOne(id);
+    if (res.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
     Resolutions.update(id, {$set: {checked: checked}});
   },
+
   deleteResolution : function(id) {
+    var res = Resolutions.findOne(id);
+    if (res.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
     Resolutions.remove(id);
+  },
+
+  setPrivate : function(id, private) {
+    var res = Resolutions.findOne(id);
+    if (res.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+    Resolutions.update(id, {$set: {private: private}});
   }
 });
