5530cf08be793c5c812ea558afd3cae92f160caa
ckhatri <chirag.k.khatri@gmail.com>
2015-07-27 10:18:14 -0500

    Added checkbox

--- resolution.js
+++ resolution.js
@@ -22,6 +22,10 @@ if (Meteor.isClient) {
   Template.resolution.events({
     "click .delete": function() {
       Resolutions.remove(this._id);
+    },
+
+    "click .toggle-checked": function() {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}});
     }
   });
 }
